d811aa3d8bccf314f2651edd77ead1250a4ae575
Stefan Fritsch <stefan.fritsch@stat-up.com>
2017-09-06 11:27:22 +0200

    * Added insert_dropdown_html() and insert_dropdown_location() for Dropdowns in RMarkdown-HTML

---
+++ R/insert_dropdown.r
@@ -0,0 +1,54 @@
+#' Insert the Necessary Code to Turn Tabs into Dropdowns
+#'
+#' RMarkdown allows you to turn subsections into tabs by putting `{.tabset}`
+#' behind the title of the parent section. With this code you can write
+#' `{.tabset .tabset-dropdown}` to get a dropdown menus instead of tabs.
+#'
+#' To use this, either create a chunk (with `results = "asis"`) that calls
+#' `insert_dropdown_html`, or put `insert_dropdown_location()` as `after_body`
+#' include in the RMarkdown.
+#'
+#' \preformatted{```{r, results = 'asis'}
+#' insert_dropdown_html()
+#' ```}
+#'
+#' or
+#'
+#' \preformatted{---
+#' title: Dropdowns
+#' output:
+#'   html_document:
+#'     includes:
+#'       after_body: `insert_dropdown_location()`
+#' ---
+#'
+#' # Header1 {.tabset .tabset-dropdown}
+#'
+#' ## Dropdown Entry 1
+#' ...
+#'
+#' ## Dropdown Entry 2
+#' ...
+#' }
+#'
+#' @export
+insert_dropdown_location <- function() {
+  x <- system.file("data", "html_document", "tabset-dropdown.html", package = "statupinternal")
+
+  stopifnot(stringr::str_length(x) > 0)
+
+  x
+}
+
+#' @rdname insert_dropdown_location
+#' @import magrittr
+#' @export
+insert_dropdown_html <- function() {
+  x <- system.file("data", "html_document", "tabset-dropdown.html", package = "statupinternal")
+
+  stopifnot(stringr::str_length(x) > 0)
+
+  x %>%
+    readLines(encoding = "UTF-8") %>%
+    cat(sep = "\n")
+}
