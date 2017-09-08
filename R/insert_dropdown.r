ccea8186b1d3316e6dbc4150ac5ff1d4e958053a
Stefan Fritsch <stefan.fritsch@stat-up.com>
2017-09-08 18:00:48 +0200

    insert_dropdown * Adding the location in the header doesn't work as planned. Fixed the documentation to reflect this. * utf-8 characters in tabset-dropdown.html fixed.

--- R/insert_dropdown.r
+++ R/insert_dropdown.r
@@ -5,21 +5,12 @@
 #' `{.tabset .tabset-dropdown}` to get a dropdown menus instead of tabs.
 #'
 #' To use this, either create a chunk (with `results = "asis"`) that calls
-#' `insert_dropdown_html`, or put `insert_dropdown_location()` as `after_body`
-#' include in the RMarkdown.
-#'
-#' \preformatted{```{r, results = 'asis'}
-#' statupinternal::insert_dropdown_html()
-#' ```}
-#'
-#' or
+#' `insert_dropdown_html`, or put the value of `insert_dropdown_location()` as
+#' `after_body` include in the RMarkdown.
 #'
 #' \preformatted{---
 #' title: Dropdowns
-#' output:
-#'   html_document:
-#'     includes:
-#'       after_body: `statupinternal::insert_dropdown_location()`
+#' output: html_document
 #' ---
 #'
 #' # Header1 {.tabset .tabset-dropdown}
@@ -29,18 +20,12 @@
 #'
 #' ## Dropdown Entry 2
 #' ...
+#'
+#' ```{r, results = 'asis', echo = FALSE}
+#' statupinternal::insert_dropdown_html()
+#' ```
 #' }
 #'
-#' @export
-insert_dropdown_location <- function() {
-  x <- system.file("data", "html_document", "tabset-dropdown.html", package = "statupinternal")
-
-  stopifnot(stringr::str_length(x) > 0)
-
-  x
-}
-
-#' @rdname insert_dropdown_location
 #' @import magrittr
 #' @export
 insert_dropdown_html <- function() {
@@ -51,4 +36,14 @@ insert_dropdown_html <- function() {
   x %>%
     readLines(encoding = "UTF-8") %>%
     cat(sep = "\n")
+}
+
+#' @rdname insert_dropdown_html
+#' @export
+insert_dropdown_location <- function() {
+  x <- system.file("data", "html_document", "tabset-dropdown.html", package = "statupinternal")
+
+  stopifnot(stringr::str_length(x) > 0)
+
+  x
 }
